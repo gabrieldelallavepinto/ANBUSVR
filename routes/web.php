427c9ceca9b49cc2f66f05cc4de8d3e998b94afd
Gabriel de la Llave Pinto <gabriel.delallavepinto@gmail.com>
2019-03-06 00:29:27 +0100

    view welcome

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,10 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::Resource('project', 'ProjectController');
