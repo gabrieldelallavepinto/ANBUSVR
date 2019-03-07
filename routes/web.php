d3b8ffaaa0948011f4fa11e9eb5ec37bf51056d3
Gabriel de la Llave Pinto <gabriel.delallavepinto@gmail.com>
2019-03-07 18:27:00 +0100

    Auth

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
 Route::Resource('project', 'ProjectController');
